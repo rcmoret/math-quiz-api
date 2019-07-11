0ddfa338a03e95d897fbd0d9feeac9bb2db723c7
Ryan's iMac <ryancmoret@gmail.com>
2019-07-11 18:32:21 -0500

    Change validations in division model

--- app/models/problem/division.rb
+++ app/models/problem/division.rb
@@ -5,6 +5,7 @@ module Problem
     validates :x_value, inclusion: (0..99)
     validates :y_value, inclusion: (1..10)
     validate :even_divisibilty!
+    validate :difficulty_level
 
     def readonly?
       false
@@ -12,9 +13,15 @@ module Problem
 
     private
 
+    def difficulty_level
+      return if x_value / y_value <= 10
+
+      errors.add(:difficulty, 'Quotient must be 10 or less')
+    end
+
     def even_divisibilty!
-      return if y_value.zero?
       return if (x_value % y_value).zero?
+
       errors.add(:values, 'Must be evenly divisible')
     end
   end
